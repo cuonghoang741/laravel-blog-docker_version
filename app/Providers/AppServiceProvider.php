5c32862ca2aac073a3a3f6cc103e023ca7f66e39
Hoàng Cường <hoangcuong@192.168.1.103>
2024-04-04 22:35:21 +0700

    update: https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
         Paginator::useBootstrap();
     }
 
